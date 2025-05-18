61d69f0bd308724b46df23071c94ea475459c993
arloor <admin@arloor.com>
2025-05-18 14:38:06 +0800

    feat: 添加测试页面路由及组件

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/text-replacer/index.tsx"), // 将文本替换页面设为首页
   route("templates", "routes/templates/index.tsx"), // 模版管理页面的路由
+  route("test", "routes/test/index.tsx"), // 测试页面的路由
 ] satisfies RouteConfig;
