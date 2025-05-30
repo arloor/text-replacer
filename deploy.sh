5c2be5b7ba37993cfbba11aafbb1fb6558b7cffc
arloor <admin@arloor.com>
2025-05-30 22:10:13 +0800

    fix: 更新部署脚本，修正 Docker 运行端口绑定为 127.0.0.1

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ ssh root@tt.arloor.com '
 docker stop stock-vite
 docker rm stock-vite
 docker pull quay.io/arloor/text-replacer:stock
-docker run --restart=always --name stock-vite -d -p 7780:3000 quay.io/arloor/text-replacer:stock
+docker run --restart=always --name stock-vite -d -p 127.0.0.1:7780:3000 quay.io/arloor/text-replacer:stock
 '
